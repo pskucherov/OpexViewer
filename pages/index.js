4e3ba7a01a2e6a6bfa8abcf0adaef7209489b023
pskucherov <pskucherov@yandex.ru>
2022-05-22 18:39:42 +0300

    index

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,13 @@
 import React from 'react';
 import Page from '../components/Page/Page';
+import { useRouter } from 'next/router';
 
 export default function Home(props) {
     const { setTitle } = props;
+    const router = useRouter();
 
     React.useEffect(() => {
         setTitle('');
-    }, [setTitle]);
-
-    // const requestOptions = {
-    //     cache: 'no-cache',
-    //     'Content-Type': 'application/json',
-    // };
-
-    // async function* readStream(stream) {
-    //     const reader = stream.getReader();
-
-    //     for (;;) {
-    //         const { value, done } = await reader.read();
-
-    //         if (value) {
-    //             yield Buffer(value).toString('utf8');
-
-    //             // console.log(Buffer(value).toString('utf8'), done);
-    //             // console.log(done);
-    //             // debugger;
-    //         }
-    //         if (done) {
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // /**
-    //  * Проверка сервера, что доступен по ссылке.
-    //  *
-    //  * @param {String} url
-    //  * @returns
-    //  */
-    // const checkServer = async url => {
-    //     let response;
-    //     const q = Buffer;
-
-    //     try {
-    //         response = await window.fetch('http://localhost:8000', {
-    //             cache: 'no-cache',
-    //         })
-    //             .then(response => response.body);
-
-    //         //.then(readStream)
-
-    //         for await (const line of readStream(response)) {
-    //             // console.log(line);
-    //         }
-
-    //         //.then(response => responseToReadable(response));
-    //     } catch (e) {
-    //         // console.log(e);
-
-    //         return false;
-    //     }
-
-    //     // debugger;
-
-    //     // if (response && response.ok) {
-    //     //     const json = await response.json();
-
-    //     //     return Boolean(json && json.status);
-    //     // }
-
-    //     return false;
-    // };
-
-    // React.useEffect(() => {
-    //     checkServer();
-    // }, [checkServer]);
-
-    return (
-        <>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-        </>
-    );
+        router.push('/instruments');
+    }, [setTitle, router]);
 }
