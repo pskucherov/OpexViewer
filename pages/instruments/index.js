08a256a1cc412622a35c1bc0db8746eb5360e370
pskucherov <pskucherov@yandex.ru>
2022-06-28 17:28:50 +0300

    миниграфики для терминала

--- pages/instruments/index.js
+++ pages/instruments/index.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { ButtonGroup, Button, CardGroup, Card, CardImg, CardBody, CardTitle, CardSubtitle, Spinner } from 'reactstrap';
-import { getInstruments } from '../../utils/instruments';
+import { getCandles, getInstruments } from '../../utils/instruments';
+import { getPrice } from '../../utils/price';
+
+import styles from '../../styles/Instruments.module.css';
 
 // Голубые фишки
 // Объём торгов внутри дня (ТОП-10)
@@ -29,6 +32,10 @@ const instrumentsButtons = [
     {
         page: 'bluechipsfutures',
         name: 'Фьючерсы',
+    },
+    {
+        page: 'shares',
+        name: 'Акции',
     },
     {
         page: 'etfs',
@@ -70,7 +77,7 @@ const SelectInstrument = props => {
     return (
         <>
             <ButtonGroup>
-                {instrumentsButtons.slice(0, 2).map((i, k) => (
+                {instrumentsButtons.map((i, k) => (
                     <Button
                         key={k}
                         color="primary"
@@ -83,7 +90,7 @@ const SelectInstrument = props => {
                     </Button>
                 ))}
             </ButtonGroup>
-            <br></br>
+            {/* <br></br>
             <ButtonGroup>
                 {instrumentsButtons.slice(2, 4).map((i, k) => (
                     <Button
@@ -97,7 +104,7 @@ const SelectInstrument = props => {
                         {i.name}
                     </Button>
                 ))}
-            </ButtonGroup>
+            </ButtonGroup> */}
             <br></br>
             {
                 inProgress ? (
@@ -106,7 +113,10 @@ const SelectInstrument = props => {
                     </Spinner></center>
                 ) : instrumenst && instrumenst.instruments ? (
                     <>
-                        <GroupInstruments instrumenst={instrumenst.instruments} />
+                        <GroupInstruments
+                            instrumenst={instrumenst.instruments}
+                            serverUri={serverUri}
+                        />
                     </>
                 ) : ''
             }
@@ -132,6 +142,7 @@ const GroupInstruments = props => {
                 name={i.name + ` (${i.ticker})`}
                 figi={i.figi}
                 key={k}
+                serverUri={props.serverUri}
             />
         );
 
@@ -156,6 +167,11 @@ const GroupInstruments = props => {
 
 const CardInstrument = props => {
     const router = useRouter();
+    const {
+        serverUri,
+        figi,
+        name,
+    } = props;
 
     return (
         <Card>
@@ -168,17 +184,31 @@ const CardInstrument = props => {
                 width="100%"
             /> */}
             <CardBody>
-                <CardTitle tag="h5">
-                    {props.name}
+                <CardTitle
+                    tag="h5"
+                    onClick={() => {
+                        router.push('/instruments/' + figi);
+                    }}
+                    style={{
+                        cursor: 'pointer',
+                        color: '#002060',
+                    }}
+                    title="Открыть терминал"
+                >
+                    {name}
                 </CardTitle>
                 <CardSubtitle
                     className="mb-2 text-muted"
                     tag="h6"
                 >
-                    {props.figi}
+                    {figi}
                 </CardSubtitle>
                 {/* <CardText>История (5 мин):</CardText> */}
-                <Button
+                <FigiBlock
+                    serverUri={serverUri}
+                    figi={figi}
+                />
+                {/* <Button
                     color="primary"
                     outline
                     onClick={() => {
@@ -186,8 +216,156 @@ const CardInstrument = props => {
                     }}
                 >
                     Терминал
-                </Button>
+                </Button> */}
             </CardBody>
         </Card>
+    );
+};
+
+const canvasWidth = 200;
+const canvasHeight = 50;
+const queue = {};
+
+const FigiBlock = props => {
+    const {
+        serverUri,
+        figi,
+    } = props;
+
+    const [data, setData] = useState();
+
+    const getChart = useCallback(async () => {
+        const to = new Date();
+        const from = new Date(to - (6 * 24 * 3600 * 1000));
+
+        const c = await getCandles(serverUri, figi, 4, from, to);
+
+        if (c && c.candles && c.candles.length) {
+            let max = getPrice(c.candles[0].high);
+            let min = getPrice(c.candles[0].low);
+
+            c.candles.forEach(c => {
+                max = Math.max(getPrice(c.high), max);
+                min = Math.min(getPrice(c.low), min);
+            });
+
+            const step = Math.floor(canvasWidth / c.candles.length);
+            const hStep = (max - min);
+
+            const d = c.candles.map((c, k) => {
+                return [
+                    k * step,
+                    (1 - (getPrice(c.close) - min) / hStep) * canvasHeight,
+
+                    // Точки для квадратичной кривой.
+                    (1 - (getPrice(c.high) - min) / hStep) * canvasHeight,
+                    (1 - (getPrice(c.low) - min) / hStep) * canvasHeight,
+
+                    // Начальная точка отрисовки
+                    (1 - (getPrice(c.open) - min) / hStep) * canvasHeight,
+                ];
+            });
+
+            d[d.length - 1][0] = canvasWidth;
+
+            setData(d);
+        } else {
+            setData([]);
+        }
+
+        queue[figi] = 1;
+    }, [figi, serverUri]);
+
+    useEffect(() => {
+        if (typeof queue[figi] === 'undefined') {
+            queue[figi] = 0;
+        }
+
+        const i = setInterval(async () => {
+            const needGetChart = false;
+
+            for (const q in queue) {
+                if (queue[q] === 2) {
+                    needGetChart = false;
+                    break;
+                } else if (!queue[q] && q === figi) {
+                    needGetChart = q;
+                } else if (queue[q] && q === figi) {
+                    clearInterval(i);
+                }
+            }
+
+            if (needGetChart) {
+                queue[needGetChart] = 2;
+                await getChart();
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(i);
+        };
+    }, [figi, getChart]);
+
+    return (
+        <div
+            className={data ? styles.EmptyBlockFilled : styles.EmptyBlock}
+            onClick={getChart}
+        >
+            { data ? (
+                <ChartCanvas
+                    data={data}
+                />
+            ) : 'Клик для загрузки' }
+        </div>
+    );
+};
+
+const ChartCanvas = props => {
+    const {
+        data,
+    } = props;
+
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        const ctx = canvasRef.current.getContext('2d');
+        const startY = data.length && data[0][4];
+
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ctx.beginPath();
+        ctx.strokeStyle = startY <= (data.length && data[data.length - 1][1]) ? 'red' : 'green';
+        ctx.moveTo(0, startY);
+
+        for (let i = 1; i < data.length; i++) {
+            const x = data[i][0];
+
+            const cp1X = Math.round((data[i - 1][0] + x) / 2);
+            let cp1Y;
+
+            // Если дата закрытия выше прошлого
+            if (data[i][1] >= data[i - 1][1]) {
+                // Корректируем через high
+                cp1Y = data[i][2];
+            } else {
+                cp1Y = data[i][3];
+            }
+
+            ctx.quadraticCurveTo(
+                cp1X,
+                cp1Y,
+                x,
+                data[i][1],
+            );
+        }
+
+        ctx.stroke();
+    }, [data]);
+
+    return (
+        <canvas
+            ref={canvasRef}
+            width={canvasWidth}
+            height={canvasHeight}
+        />
     );
 };
